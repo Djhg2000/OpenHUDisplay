8f7d32467596a2aab27f136afcecc5ea0517f595
Djhg2000 <github@djhg2000.net>
2010-11-26 06:42:44 +0100

    Fixed stupid errors in the generate_*.sh scripts

--- generate_archive.sh
+++ generate_archive.sh
@@ -6,5 +6,6 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 echo "Compressing..."
-tar -vzcf hud.tar.gz project_pic16f690.c speed2string.c delays.h delays.c br-355.c m12by02aa.c i2c.c project_pic16f690.hex generate_archive.sh
+tar -vzcf hud.tar.gz project_pic16f690.c speed2string.c br-355.c m12by02aa.c i2c.c project_pic16f690.hex generate_archive.sh
 echo "Done. Output archive is hud.tar.gz"
+
--- generate_hex.sh
+++ generate_hex.sh
@@ -6,35 +6,35 @@ if [ -e project_pic16f690.c ]; then
 	echo "yes"
 else
 	echo "no"
-	exit
+	exit 1
 fi
 echo -n "speed2string.c..."
 if [ -e speed2string.c ]; then
         echo "yes"
 else
         echo "no"
-        exit
+        exit 1
 fi
 echo -n "br-355.c..."
 if [ -e br-355.c ]; then
         echo "yes"
 else
         echo "no"
-        exit
+        exit 1
 fi
 echo -n "m12by02aa.c..."
 if [ -e m12by02aa.c ]; then
         echo "yes"
 else
         echo "no"
-        exit
+        exit 1
 fi
 echo -n "i2c.c..."
 if [ -e i2c.c ]; then
         echo "yes"
 else
 	echo "run patch_i2c.sh"
-	exit
+	exit 1
 fi
 echo -n "cc5x..."
 if [ `which cc5x` ]; then
@@ -42,7 +42,7 @@ if [ `which cc5x` ]; then
 else
         echo "no"
 	echo "Make sure cc5x is installed and the wrapper script for wine is located at $HOME/bin/cc5x"
-        exit
+        exit 1
 fi
 
 echo "Compiling..."
